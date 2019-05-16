8d7307e4e262e344635fd3841296c3c1abfa5c94
Radames S. Cruz Moreno <v-rcruz@microsoft.com>
2019-05-16 21:20:46 +0000

    Updated script

--- install.sh
+++ install.sh
@@ -1,2 +1,86 @@
 #!/bin/bash
 
+# Parallel make
+makeparallel=-j
+
+sudo apt install cmake
+sudo apt install g++
+sudo apt install unzip
+sudo apt install libtclap-dev
+sudo apt install libboost-dev
+sudo apt install libmsgsl-dev
+sudo apt install libflint-dev
+sudo apt install liblog4cplus-dev
+sudo apt install libzmqpp-dev
+
+mkdir -p ~/projects
+cd ~/projects
+
+# FourQ
+git clone https://github.com/kiromaru/FourQlib.git
+
+cd FourQlib/FourQ_64bit_and_portable
+make $makeparallel
+./install.sh
+
+
+# Crypto++ 7.0
+cd ~/projects
+mkdir -p cryptopp
+cd cryptopp
+curl https://cryptopp.com/cryptopp700.zip --output cryptopp700.zip
+unzip cryptopp700.zip
+
+make $makeparallel
+sudo make install
+
+# Cuckoo
+cd ~/projects/Cuckoo/Cuckoo
+mkdir -p .build
+cd .build
+cmake .. -DCMAKE_INSTALL_PREFIX=~/mylibs
+make $makeparallel
+make install
+
+# SEAL
+cd ~/projects/SEAL/native/src
+mkdir -p .build
+cd .build
+cmake .. -DCMAKE_INSTALL_PREFIX=~/mylibs
+make $makeparallel
+make install
+
+# APSI
+cd ~/projects/APSI/APSICommon
+mkdir -p .build
+cd .build
+cmake .. -DCMAKE_INSTALL_PREFIX=~/mylibs
+make $makeparallel
+make install
+
+cd ~/projects/APSI/APSISender
+mkdir -p .build
+cd .build
+cmake .. -DCMAKE_INSTALL_PREFIX=~/mylibs
+make $makeparallel
+make install
+
+cd ~/projects/APSI/APSIReceiver
+mkdir -p .build
+cd .build
+cmake .. -DCMAKE_INSTALL_PREFIX=~/mylibs
+make $makeparallel
+make install
+
+cd ~/projects/APSI/SenderCLI
+mkdir -p .build
+cd .build
+cmake .. -DCMAKE_PREFIX_PATH=~/mylibs
+make $makeparallel
+
+cd ~/projects/APSI/ReceiverCLI
+mkdir -p .build
+cd .build
+cmake .. -DCMAKE_PREFIX_PATH=~/mylibs
+make $makeparallel
+
